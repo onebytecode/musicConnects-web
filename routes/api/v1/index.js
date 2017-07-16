4e645359d4f94fb05997dbb0f7eb6bf17f073bdd
goncharovnikita <goncharovnikita.dev@gmail.com>
2017-07-16 17:07:03 +0300

    Api methods complete: user registrate user authenticate

---
+++ routes/api/v1/index.js
@@ -0,0 +1,5 @@
+// ROOT API V1
+
+module.exports = (r, c) => {
+  return require('./api_v1')(r, c)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 module.exports =  (router, controllers) => {
-  require('./router')(router, controllers)
+  const commonRouter = require('./router')(router, controllers)
+  const api_v1 = require('./api')(router, controllers).v1
+
+  const routers = {
+    commonRouter: commonRouter,
+    api_v1: api_v1
+  }
+  return routers
 }
