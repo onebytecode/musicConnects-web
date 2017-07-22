698474f3a1db75e603efcae1e9293fa70add0a4a
goncharovnikita <goncharovnikita.dev@gmail.com>
2017-07-22 12:19:01 +0300

    mongo path fixed

--- config/db/db.js
+++ config/db/db.js
@@ -3,8 +3,10 @@ const ENV  =  process.env.NODE_ENV || 'dev'
 const getUrl = () => {
   if (ENV === 'test') {
     return "mongodb://localhost:27017/test"
+  } else if (ENV === 'production') {
+    return "mongodb://mongoDB:27017/music_hub"
   } else {
-    return "mongodb://localhost:27017/music_hub"
+    return "mongodb://localhost:27017/music_hub_dev"
   }
 }
 
